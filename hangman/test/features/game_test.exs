02ecf7c1c82ae8d823539d0ddea1fff074095efc
Jonathan Allen <jallen@snow.edu>
2019-11-07 12:26:21 -0700

    Finish next module

--- hangman/test/features/game_test.exs
+++ hangman/test/features/game_test.exs
@@ -4,26 +4,19 @@ defmodule Hangman.GameCabbageTest do
 
   setup do
     on_exit fn -> IO.puts "on_exit event raised.  Scenario testing is complete" end
-
   end
 
   defgiven ~r/I have a new game with the word (?<word>.*)/, %{word: word}, %{} do
     game = Game.new_game(word)
-    IO.puts "In the 'given'"
-    IO.inspect game
     {:ok, %{game: game}}
   end
 
   defwhen ~r/I guess (?<guess>.*)/, %{guess: guess}, %{game: game} do
     game = Game.make_move(game, guess)
-    IO.puts "In the 'when'"
-    IO.inspect game
     {:ok, %{game: game}}
   end
 
-  defthen ~r/^the number of turns left is (?<expected>\d+)$/, %{expected: expected_str}, %{game: {game, _tally}} do
-    IO.puts "Expected: #{expected_str}"
-    IO.inspect game
+  defthen ~r/^the number of turns left is (?<expected>\d+)$/, %{expected: expected_str}, %{game: game} do
     {expected, _} = Integer.parse(expected_str)
     assert game.turns_left == expected
   end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -17,44 +17,53 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [ :won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert {^game, _} = Game.make_move(game, "x")
+      assert ^game = Game.make_move(game, "x")
     end
   end
 
   test "first occurrence of letter is not already used" do
     game = Game.new_game()
-    {game, _tally} = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
 
-    {game, _tally} = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wribble")
-    {game, _tally} = Game.make_move(game, "w")
+    game = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a guessed word is a won game" do
     game = Game.new_game("wribble")
-    {game, _tally} = Game.make_move(game, "w")
-    {game, _tally} = Game.make_move(game, "r")
-    {game, _tally} = Game.make_move(game, "i")
-    {game, _tally} = Game.make_move(game, "b")
-    {game, _tally} = Game.make_move(game, "b")
-    {game, _tally} = Game.make_move(game, "l")
-    {game, _tally} = Game.make_move(game, "e")
+    game = Game.make_move(game, "w")
+    game = Game.make_move(game, "r")
+    game = Game.make_move(game, "i")
+    game = Game.make_move(game, "b")
+    game = Game.make_move(game, "b")
+    game = Game.make_move(game, "l")
+    game = Game.make_move(game, "e")
     assert game.game_state == :won
     assert game.turns_left == 7
   end
 
   test "bad guess is recognized" do
     game = Game.new_game("wibble")
-    {game, _tally} = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
 
+  test "bad guess runs out of guesses" do
+    game = Game.new_game("w")
+    game = Game.make_move(game, "a")
+    IO.inspect game
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
 end
+
+
